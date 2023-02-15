d5c3c8d45daed9550e550db2bf71390ad3bc8780
Camila Pinzon <camilapinz18@gmail.com>
2023-02-15 11:18:04 -0500

    asigancion nuevos valores
    -se asigna precio aleatorio,
    -se asigna estado aleatorio,
    - renderizado condicional boton segun estado

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
     return {
       pages: null,
       selectedPage: null,
-      charactersList: []
+      charactersList: [],
+      characters: []
     }
   },
   methods: {
@@ -12,17 +13,34 @@ const app = Vue.createApp({
     },
     async fetchData () {
       let page = this.assignRandomPage()
-      console.log("Page",page)
+      console.log('Page', page)
       try {
         const response = await fetch(
           `https://rickandmortyapi.com/api/character/?page=${page}`
         )
-        const characters = await response.json()
+        const chars = await response.json()
+
+        this.characters = chars.results
+
         this.charactersList = characters.results
-        this.pages = characters.info.pages
       } catch (error) {
         console.log(error)
       }
+
+      this.characters.map(char => {
+        this.charactersList.push({
+          name: char.name,
+          image: char.image,
+          species: char.species,
+          status: char.status,
+          price: this.assignRandomPrice(),
+          cardStatus:this.assignRandomCardStatus()
+        })
+      })
+
+      console.log('charasedit', this.charactersList)
+
+      localStorage.setItem('cards', JSON.stringify(this.charactersList))
     },
     addToCart (char) {
       console.log(char)
@@ -31,6 +49,20 @@ const app = Vue.createApp({
     assignRandomPage () {
       let randPage = Math.round(Math.random() * 42)
       return randPage
+    },
+    assignRandomPrice () {
+      let minPrice = 20
+      let maxPrice = 500
+      let randPrice = Math.floor(
+        Math.random() * (maxPrice - minPrice + 1) + minPrice
+      )
+
+      return randPrice
+    },
+    assignRandomCardStatus () {
+      const cardStatus = ['inAuction', 'forSale']
+      let randStatus = cardStatus[Math.floor(Math.random() * cardStatus.length)]
+      return randStatus
     }
   },
 
